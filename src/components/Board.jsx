95456280d4b9f0d2535e60bee60fa2941333c064
mattviolet <mattviolet@gmail.com>
2019-10-12 15:10:49 -0700

    project setup

---
+++ src/components/Board.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h1>Board</h1>
+      )
+  }
+}
+
+export default Board;
