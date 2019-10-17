2c85abbc536ef2e2bde738f59c838e1e9884a19a
mattviolet <mattviolet@gmail.com>
2019-10-16 20:30:58 -0700

    Add css animations for moves

--- src/components/GridCell.jsx
+++ src/components/GridCell.jsx
@@ -7,22 +7,30 @@ class GridCell extends React.Component {
       cellId: [this.props.colId, this.props.cellId],
       color: this.props.color
     }
+    this.generateKey = this.generateKey.bind(this);
   }
 
   componentDidUpdate() {
     this.props.callCheckForWinner();
   }
 
+  generateKey() {
+    let cellId = this.state.cellId;
+    let col = cellId[0].toString();
+    let cell = cellId[1].toString();
+    let key = col.concat(cell);
+    return key;
+  }
+
   render() {
     return (
-      <div>
+      <div className='gridCellnoColor'>
         <button 
           className={'gridCell' + this.props.color} 
           id={this.state.cellId}
           value={this.props.color} 
           onClick={this.props.makeMove} 
         />
-        <br/>
       </div>
     )
   }
